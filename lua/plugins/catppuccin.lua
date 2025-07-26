c0629f69935753788bed515338bcd26441d81e40
zac <zacariatawfick@gmail.com>
2025-07-26 13:44:26 -0400

    Changed color.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000, -- make sure it loads early
   config = function()
     require("catppuccin").setup({
-      flavour = "frappe", -- latte, frappe, macchiato, mocha
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
       -- You can add more catppuccin config here if you want
       -- e.g., background = { light = "latte", dark = "mocha" }
     })
