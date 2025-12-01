4ea8e3803846f9de074a320184e4f72d2e8fecb3
Zac <zacariatawfick@gmail.com>
2025-11-30 19:08:40 -0500

    Make indentation consistent.

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+-- Set indentation to 4 spaces
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
