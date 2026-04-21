2026ac6f5c6ad96268457d93959761eba906bb37
Zac <zacariatawfick@gmail.com>
2026-04-20 22:19:02 -0400

    Fix treesitter conflict.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,13 +3,13 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- Customize Treesitter
 
 ---@type LazySpec
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
-}
+-- return {
+--   "nvim-treesitter/nvim-treesitter",
+--   opts = {
+--     ensure_installed = {
+--       "lua",
+--       "vim",
+--       -- add more arguments for adding more treesitter parsers
+--     },
+--   },
+-- }
